5d0b59d64a8e64829652c47b04fbc70d76f0a8b7
Quang Trinh <quang.trinh@gmail.com>
2016-10-31 06:13:41 -0400

    added check_external_commands.pl

---
+++ bin/check_external_commands.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+
+my @tools = ("java", "tabix");
+my $tool_path = '';
+my $foundTool;
+
+print "\n\nChecking dependencies for ISOWN ... \n\n";
+	
+foreach my $t (0 .. $#tools) {
+	$foundTool = 0;
+		
+	my $tool_name = $tools[$t];
+	for my $path ( split /:/, $ENV{PATH} ) {
+		if ( -f "$path/$tool_name" && -x _ ) {
+			$foundTool = 1;
+			print "$tool_name found in $path\n\n";
+			#$tool_path = "$path/$tool_name";
+			last ;
+		}
+	}
+	if ($foundTool == 0) {
+		print "$tool_name does not exist or not in the path!  Please install $tool_name  ...\n\n";
+	}
+}
+print "\n\n";
